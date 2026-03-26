076cf19b22e2dc0868e00248468694a75e1247b5
Weipu Zhu <zhuweipu99@qq.com>
2026-03-26 12:53:30 +0800

    Update mixed workload test and performance analysis report
    - Updated wrk.lua script to implement 70% write (POST) + 30% read (GET) mixed workload
    - Updated docs/performance_analysis.md performance analysis report with mixed workload test results and analysis

--- scripts/wrk.lua
+++ scripts/wrk.lua
@@ -1,7 +1,16 @@
-wrk.method = "POST"
+-- 70% write (POST), 30% read (GET)
 wrk.body   = '{"message": "Hello World!"}'
 wrk.headers["Content-Type"] = "application/json"
 
 request = function()
-   return wrk.format(nil, nil, nil)
+   local r = math.random()
+   if r < 0.7 then
+      -- 70% POST (write)
+      wrk.method = "POST"
+      return wrk.format("POST", nil, wrk.headers, wrk.body)
+   else
+      -- 30% GET (read)
+      wrk.method = "GET"
+      return wrk.format("GET", nil, wrk.headers, nil)
+   end
 end
